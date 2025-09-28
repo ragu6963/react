967adf2ff0625406a9962792178c1630a5551734
Beemo <bhj1684@naver.com>
2025-09-28 23:33:36 +0900

    파일 경로 수정 및 불필요한 컴포넌트 삭제: QuoteContainer, PostList, Post, Quote 컴포넌트를 제거하고, App.jsx에서 해당 경로를 수정함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Calculator from "./components/Practice/Calculator/Calculator";
-import QuoteContainer from "./components/DummyJSON/QuoteContainer";
+import QuoteContainer from "./components/Practice/DummyJSON/QuoteContainer";
 import NoDependency from "./components/Learning/UseEffect/NoDependency";
 import InfiniteLoop from "./components/Learning/UseEffect/InfiniteLoop";
 import UserInfo from "./components/Learning/UseEffect/UserInfo";
-import PostList from "./components/DataFetching/PostList";
+import PostList from "./components/Learning/DataFetching/PostList";
 import RecipeContainer from "./components/Practice/Recipe/RecipeContainer";
 export default function App() {
   return (
